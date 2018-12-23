102b827d2ead32cf7772932622a56479e8e22dd4
Nihal Soans <nihalsoans91@gmail.com>
2018-12-23 15:57:42 -0500

    First commit

---
+++ src/rem_noise.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import time
+
+image = cv2.imread("sample.jpg",0)
+# we have to ignore 0 and 127 the only issue is from 255
+image[image<255] = 0
+
+cv2.imwrite("postproc.jpg",image)
+w=image.shape[0]
+h=image.shape[1]
+print(w)
+print(h)
+image = cv2.imread("sample.jpg",0)
+image = cv2.imread("sample.jpg",0)
+k=16 # this is the kernel size
+k2=2 # this isthe inner kernel size
+start_time = time.time()
+filter_gap=int((k/2)-1)
+window_right=w-k
+window_down=h-k
+for d in range(window_down):
+    for r in range(window_right):
+        #det_filter=image[int(k*r):int(k*(r+1)),int(k*r):int(k*(r+1))]
+        outer_filter=np.copy(image[d:d+k,r:r+k]) #ymin,ymax
+        inner_filter=np.copy(outer_filter[filter_gap:filter_gap+k2,filter_gap:filter_gap+k2])
+        if np.max(inner_filter) == 255:
+            outer_filter[filter_gap:filter_gap+k2,filter_gap:filter_gap+k2]=0
+            if np.max(outer_filter) == 255:
+                continue
+            else:
+                image[d:d+k,r:r+k]=outer_filter
+                cv2.imwrite("right\conv"+str(r)+str(d)+".jpg",image)
+        else:
+            continue
+print("--- %s seconds ---" % (time.time() - start_time))
